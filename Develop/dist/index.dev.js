faff1b8c3bbdeee64f5af508d5304696ec3b343b
jennifer gomez <jennifer23gomez7@gmail.com>
2021-02-14 18:02:20 -0800

    added npm package, functionallity of questions are prompt using node and wrote to new file

---
+++ Develop/dist/index.dev.js
@@ -0,0 +1,106 @@
+"use strict";
+
+// TODO: Include packages needed for this application
+var fs = require("fs");
+
+var inquirer = require("inquirer");
+
+var util = require("util");
+
+var generateReadme = require("./utils/generateMarkdown");
+
+var writeFileAsync = util.promisify(fs.writeFile); // TODO: Create an array of questions for user input
+// const questions = []; convert to ES6+
+
+function questions() {
+  return inquirer.prompt([{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+  }, {
+    type: "input",
+    name: "description",
+    message: "Please write a breif description of your project."
+  }, {
+    type: "input",
+    name: "purpose",
+    message: "What is the purpose of your project?"
+  }, {
+    type: "input",
+    name: "installation",
+    message: "Please describe what intallation process did you use for your project."
+  }, {
+    type: "checkbox",
+    name: "license",
+    message: "Please choose the appropriate license for your project.",
+    choices: ["Academic", "GNU", "ISC", "Open"]
+  }, {
+    type: "input",
+    name: "contributing",
+    message: "Who are the contributors to this project?"
+  }, {
+    type: 'list',
+    message: 'What is your preferred method of communication?',
+    name: 'contact',
+    choices: ['email', 'github', 'slack']
+  }, {
+    type: "input",
+    name: "email",
+    message: "Please enter your email: "
+  }, {
+    type: "input",
+    name: "username",
+    message: "Please enter your GitHub username: "
+  }, {
+    type: "input",
+    name: "slackname",
+    message: "Please enter your Slack username: "
+  }, {
+    type: "input",
+    name: "tests",
+    message: "Were there any tests included in this project?"
+  }]);
+}
+
+; // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+// TODO: Create a function to initialize app  ;  added a promise 
+
+function init() {
+  var answers, generateData;
+  return regeneratorRuntime.async(function init$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(questions());
+
+        case 3:
+          answers = _context.sent;
+          // The await is used to wait for the promise.
+          generateData = generateReadme(answers);
+          _context.next = 7;
+          return regeneratorRuntime.awrap(writeFileAsync('./dist/README.md', generateData));
+
+        case 7:
+          //to display in folder
+          console.log("✔️  Successfully wrote to README.md'");
+          _context.next = 13;
+          break;
+
+        case 10:
+          _context.prev = 10;
+          _context.t0 = _context["catch"](0);
+          console.log(_context.t0);
+
+        case 13:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, [[0, 10]]);
+} // Function call to initialize app
+
+
+init();
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,94 @@
 // TODO: Include packages needed for this application
 
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const generateReadme = require("./utils/generateMarkdown")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = []; convert to ES6+
+function questions() {
+    return inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a breif description of your project."
+        },
+        {
+            type: "input",
+            name: "purpose",
+            message: "What is the purpose of your project?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please describe what intallation process did you use for your project."
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "Please choose the appropriate license for your project.",
+            choices: ["Academic", "GNU", "ISC", "Open"]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors to this project?"
+        },
+        {
+            type: 'list', 
+            message: 'What is your preferred method of communication?',
+            name: 'contact',
+            choices: ['email', 'github', 'slack']
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "slackname",
+            message: "Please enter your Slack username: "
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Were there any tests included in this project?"
+        }
+    ]);
+};
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// TODO: Create a function to initialize app  ;  added a promise 
+async function init() {
+    // usin try to mark the blockof statements and specifies a response
+    try {
+        const answers = await questions(); // The await is used to wait for the promise.
+        const generateData = generateReadme(answers);
+        await writeFileAsync('./dist/README.md', generateData); //to display in folder
+            console.log ("✔️  Successfully wrote to README.md'");
+    } catch(err) {
+        console.log(err);
+    }
+}
 
 // Function call to initialize app
 init();
